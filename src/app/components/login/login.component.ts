d14b69e343c9c2fc1f4f0487e6debf224fee076d
GHILENISABEUR <ghilanisaber191@gmail.com>
2025-02-01 13:20:11 +0100

    hide and show password function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: false,
-  
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  type:string="password";
+  isText:boolean=false;
+  eyeIcon:string="fa-eye-slash";
+
+
+  constructor(){}
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon="fa-eye":this.eyeIcon="fa-eye-slash";
+    this.isText ? this.type="text" :this.type="password"
+
+
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
 
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   standalone: false,
-  
+
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  type:string="passowrd";
+  isText:boolean=false;
+  eyeIcon:string="fa-eye-slash";
+  constructor(){
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon="fa-eye":this.eyeIcon="fa-eye-slash";
+    this.isText ?this.type="text" : this.type="password";
+
+  }
 
 }
