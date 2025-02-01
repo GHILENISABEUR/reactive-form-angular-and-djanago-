51096f9b8b240c9aaca444c15468f3b3eb579f2f
GHILENISABEUR <ghilanisaber191@gmail.com>
2025-02-01 12:21:56 +0100

    added login and signup pages and linked them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelpComponent } from './helpers/help/help.component';
+import { ModelComponent } from './models/model/model.component';
+import { ServiceComponent } from './services/service/service.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpComponent,
+    ModelComponent,
+    ServiceComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterLink,
   ],
   providers: [],
   bootstrap: [AppComponent]
